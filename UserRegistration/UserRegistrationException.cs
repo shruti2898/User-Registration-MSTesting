e21655c4cd50778d52ea8cdf0666fc5bf6d593de
Shruti <shrutisablaniya93@gmail.com>
2021-10-07 22:17:36 +0530

    User Registration and Customized exception handling

---
+++ UserRegistration/UserRegistrationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    public class UserRegistrationException : Exception
+    {
+        public enum ExceptionType
+        {
+            Empty_Message,
+            NULL_Message
+        }
+        private readonly ExceptionType type;
+
+        public UserRegistrationException(ExceptionType Type, String message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
